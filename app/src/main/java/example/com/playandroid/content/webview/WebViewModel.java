9576758c8e6f4d29b13637507235e0c529de5fc5
wyb <2088857969@qq.com>
2018-12-28 17:31:45 +0800

    会去看动画吧 ~！@#￥%￥……%

--- app/src/main/java/example/com/playandroid/content/webview/WebViewModel.java
+++ app/src/main/java/example/com/playandroid/content/webview/WebViewModel.java
@@ -5,13 +5,15 @@ import android.view.View;
 import example.com.playandroid.base.BaseModel;
 import example.com.playandroid.constant.Constant;
 import example.com.playandroid.databinding.ActivityWebviewBinding;
+import example.com.playandroid.util.AnimUtil;
 
 /**
  * @author admin
  * @des 2018/10/25
  */
-public class WebViewModel extends BaseModel<WebViewActivity,ActivityWebviewBinding> {
-    private String title="玩安卓";
+public class WebViewModel extends BaseModel<WebViewActivity, ActivityWebviewBinding> {
+    private String title = "玩安卓";
+
     public WebViewModel(WebViewActivity activity) {
         super(activity);
     }
@@ -19,6 +21,7 @@ public class WebViewModel extends BaseModel<WebViewActivity,ActivityWebviewBindi
     @Override
     public void onCreate() {
         super.onCreate();
+        AnimUtil.initPopLayout(getBinding().fab, getBinding().backDrop, getBinding().llLink, getBinding().llBrowser, getBinding().llLike);
         String link = getActivity().getIntent().getStringExtra(Constant.link);
         title = getActivity().getIntent().getStringExtra(Constant.article_title);
         getBinding().webView.loadUrl(link);
@@ -31,6 +34,7 @@ public class WebViewModel extends BaseModel<WebViewActivity,ActivityWebviewBindi
     public void setTitle(String title) {
         this.title = title;
     }
+
     public void navigationClick(View view) {
         getActivity().finish();
     }
--- app/src/main/java/example/com/playandroid/util/AnimUtil.java
+++ app/src/main/java/example/com/playandroid/util/AnimUtil.java
@@ -1,23 +1,30 @@
 package example.com.playandroid.util;
 
+import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
+
+import timber.log.Timber;
 
 /**
  * @author Richard_Y_Wang
  * @des 2018/11/11 15:24
  */
 public class AnimUtil {
+    private static boolean rotate = false;
+
     /**
-     *  这种写法是旋转到某个角度 比如写的是rotation 90 是旋转到90度
-     *  如果使用rotationBy 比如写的是rotationBy 90 是顺时针旋转90度
-     *  使view 旋转某个角度 中心旋转
+     * 这种写法是旋转到某个角度 比如写的是rotation 90 是旋转到90度
+     * 如果使用rotationBy 比如写的是rotationBy 90 是顺时针旋转90度
+     * 使view 旋转某个角度 中心旋转
+     *
      * @param view  被旋转的控件
      * @param time  动画时间
      * @param angle 旋转角度
      */
-    public static void rotate(View view,long time,float angle,AnimatorListenerAdapter listener) {
+    public static void rotate(View view, long time, float angle, AnimatorListenerAdapter listener) {
         view.animate()
                 .setDuration(time)   //设置动画时间
                 .rotation(angle)      //设置旋转角度 是旋转到某个角度
@@ -27,22 +34,109 @@ public class AnimUtil {
     }
 
     /**
-     *
-     * @param view 被旋转的控件
-     * @param isXAxis 是不是关于X轴 旋转
-     * @param time 动画时间
-     * @param angle 角度
-     * @param listener  监听器回调
+     * @param view     被旋转的控件
+     * @param isXAxis  是不是关于X轴 旋转
+     * @param time     动画时间
+     * @param angle    角度
+     * @param listener 监听器回调
      */
-    public static void rotateByAxis(View view,boolean isXAxis,long time,float angle,AnimatorListenerAdapter listener) {
+    public static void rotateByAxis(View view, boolean isXAxis, long time, float angle, AnimatorListenerAdapter listener) {
         ViewPropertyAnimator v = view.animate().setDuration(time);
-        if (isXAxis) v.rotationX(angle);
-        else v.rotationY(angle);
+        if (isXAxis)
+            v.rotationX(angle);
+        else
+            v.rotationY(angle);
         v.setListener(listener).start();
     }
 
-    public static void moveAnd(View view,long time,float alpha,int y) {
+    public static void moveAnd(View view, long time, float alpha, int y) {
         view.animate().setDuration(time).y(y).start();
     }
 
+
+    public static void initShowOut(final View v) {
+        v.setVisibility(View.GONE);
+        v.setTranslationY(v.getHeight());
+        v.setAlpha(0f);
+    }
+
+    public static void initPopLayout(FloatingActionButton fab, View backDrop, View... popLayouts) {
+        rotate = false;
+        if (popLayouts == null || popLayouts.length == 0 || backDrop == null)
+            return;
+
+        for (View ll : popLayouts) {
+            initShowOut(ll);
+        }
+        fab.setOnClickListener(v -> toggleFabMode(fab, popLayouts, backDrop));
+        backDrop.setOnClickListener(v -> toggleFabMode(fab, popLayouts, backDrop));
+
+    }
+
+    private static void toggleFabMode(View v, View[] popLayouts, View backDrop) {
+
+        rotate = rotateFab(v, !rotate);
+        v.setTag(rotate);
+        if (rotate) {
+            for (View ll : popLayouts) {
+                showIn(ll);
+            }
+            backDrop.setVisibility(View.VISIBLE);
+        } else {
+            for (View ll : popLayouts) {
+                showOut(ll);
+            }
+            backDrop.setVisibility(View.GONE);
+        }
+    }
+
+    public static boolean rotateFab(final View v, boolean rotate) {
+        v.animate().setDuration(200)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                    }
+                })
+                .rotation(rotate ? 135f : 0f);
+        return rotate;
+    }
+
+    public static void showIn(final View v) {
+        Timber.i("showIn start!!!!!top:%1s,right:%2s,bottom:%3s,left:%4s", v.getTop(), v.getRight(), v.getBottom(), v.getLeft());
+        v.setVisibility(View.VISIBLE);
+        v.setAlpha(0f);
+        v.setTranslationY(v.getHeight());
+        v.animate()
+                .setDuration(200)
+                .translationY(0)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        Timber.i("showIn end!!!!!top:%1s,right:%2s,bottom:%3s,left:%4s", v.getTop(), v.getRight(), v.getBottom(), v.getLeft());
+                    }
+                })
+                .alpha(1f)
+                .start();
+    }
+
+    public static void showOut(final View v) {
+        Timber.i("showOut start!!!!!top:%1s,right:%2s,bottom:%3s,left:%4s", v.getTop(), v.getRight(), v.getBottom(), v.getLeft());
+        v.setVisibility(View.VISIBLE);
+        v.setAlpha(1f);
+        v.setTranslationY(0);
+        v.animate()
+                .setDuration(200)
+                .translationY(v.getHeight())
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Timber.i("showOut end!!!!!top:%1s,right:%2s,bottom:%3s,left:%4s", v.getTop(), v.getRight(), v.getBottom(), v.getLeft());
+                        v.setVisibility(View.GONE);
+                        super.onAnimationEnd(animation);
+                    }
+                }).alpha(0f)
+                .start();
+    }
 }
